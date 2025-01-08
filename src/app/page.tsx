b4988f755b9ec9d323582e58bb8b765198b92ded
1top10000 <107670523+1top10000@users.noreply.github.com>
2025-01-08 09:37:18 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 'use client'
 
 export default function Home() {
-  const r = async () => {
-    fetch('http://localhost:3000/api/form', {
-      method: "POST",
-          body: JSON.stringify({
-              i: 11
-          })
-    });
-  }
   return (
     <>
+      67
     </>
   );
 }
